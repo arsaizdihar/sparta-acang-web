04f692e94572ca23c0e71bafedb317f33a6298e3
maikeljh <mjh191003@gmail.com>
2022-08-25 22:00:39 +0700

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <>
+      <div className="py-10 bg-sudo-grad1 text-sudo-tan flex flex-col gap-4 items-center w-full">
+        <h1 className="sm:text-[64px] text-[32px] font-sudo-title">
+          LOGO SUDOVERSE
+        </h1>
+        <p className="text-xl font-sudo-body">created 2022</p>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
