13642b826a5f6be6962b93645580679c29a6d148
arsaizdihar <arsadihar@gmail.com>
2022-08-26 22:27:01 +0700

    Add vote route

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,10 +2,12 @@
 import superjson from 'superjson';
 import { createRouter } from './context';
 import { eventRouter } from './event';
+import { milestoneRouter } from './milestone';
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge('', eventRouter);
+  .merge('', eventRouter)
+  .merge('milestone.', milestoneRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
