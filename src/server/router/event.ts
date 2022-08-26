36413a9d7d82052c97dd9655f0a1c39f8cd641b8
arsaizdihar <arsadihar@gmail.com>
2022-08-26 22:05:09 +0700

    Tweak event register algorithm

--- src/server/router/event.ts
+++ src/server/router/event.ts
@@ -13,25 +13,61 @@ export const eventRouter = createProtectedRouter().mutation('registerEvent', {
       throw new TRPCError({ code: 'NOT_FOUND', message: 'Event not found' });
     }
 
-    const participantCount = await ctx.prisma.participation.aggregate({
-      where: {
-        eventId: event.id,
-      },
-      _count: {
-        userId: true,
-      },
-    });
     const user = ctx.session.user;
 
-    const quotaTotal = event.quota19 + event.quota20 + event.quota21;
+    // disallow class of 2018
+    if (user.classYear === 18) {
+      throw new TRPCError({
+        code: 'FORBIDDEN',
+        message: 'Only class of 2019, 2020, and 2021 are allowed.',
+      });
+    }
 
-    if (participantCount._count.userId < quotaTotal) {
-      return await ctx.prisma.participation.create({
+    // get participant count for each class year
+    const participantCount = await ctx.prisma.user.groupBy({
+      by: ['classYear'],
+      where: {
+        participation: { isNot: null },
+      },
+      _count: true,
+    });
+
+    // participants quota for each class
+    const classQuota = {
+      21: event.quota21,
+      20: event.quota20,
+      19: event.quota19,
+    };
+
+    // participants count for each class
+    const classCount = {
+      21: event.quota21,
+      20: event.quota20,
+      19: event.quota19,
+    };
+
+    // count the 'non waiting' participants for each class
+    for (const { _count, classYear } of participantCount) {
+      // @ts-ignore
+      classCount[classYear] = min(_count, classCount[classYear]);
+    }
+
+    const quotaTotal = Object.values(classQuota).reduce((a, b) => a + b, 0);
+
+    // total 'non waiting' participants
+    const quotaUsed = Object.values(classCount).reduce((a, b) => a + b, 0);
+
+    if (quotaUsed < quotaTotal) {
+      await ctx.prisma.participation.create({
         data: {
           userId: user.id,
           eventId: event.id,
         },
       });
+      const isWaiting =
+        classCount[user.classYear] <= classQuota[user.classYear];
+
+      return { isWaiting };
     } else {
       throw new TRPCError({
         code: 'BAD_REQUEST',
