ad110543d6a28fe0578c701978b4419e4698c098
Fatih20 <fnri39@protonmail.com>
2022-08-27 20:13:16 +0700

    Initial work on page layout.

---
+++ src/pages/sudoex.tsx
@@ -0,0 +1,11 @@
+type Props = {};
+
+const SudoEx = (props: Props) => {
+  return (
+    <div className="w-full">
+      <div className="w-full max-w-5xl grid grid-cols-1 lg:grid-cols-2 auto-rows-min"></div>
+    </div>
+  );
+};
+
+export default SudoEx;
