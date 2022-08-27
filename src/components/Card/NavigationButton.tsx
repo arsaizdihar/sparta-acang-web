64433eea02dd1ac919ce552ad5eff8ddb5758105
Fatih20 <fnri39@protonmail.com>
2022-08-27 18:35:52 +0700

    Responsive design on the navigation button.

--- src/components/Card/NavigationButton.tsx
+++ src/components/Card/NavigationButton.tsx
@@ -15,7 +15,7 @@ const NavigationButton = ({
       disabled={disabled}
       className={`image-gallery-icon image-gallery-${
         isLeft ? 'left' : 'right'
-      }-nav text-2xl py-2 px-3 flex items-center justify-center rounded-full bg-sudo-dark-brown opacity-75 ${
+      }-nav text-lg sm:text-2xl py-1 px-2 sm:py-2 sm:px-3 flex items-center justify-center rounded-full bg-sudo-dark-brown opacity-75 ${
         isLeft ? 'ml-3' : 'mr-3'
       } text-sudo-dark-tan`}
       onClick={onClick}
